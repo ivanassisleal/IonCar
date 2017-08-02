50885bff0dfdb9dc6af13ff4cc89dd0f8ef8c5a5
ivanassisleal <ivanassisleal@gmail.com>
2017-08-02 13:36:14 -0300

    Add Controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule  } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @NgModule({
   declarations: [
@@ -14,6 +18,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public carros;
 
+  constructor(
+    public navCtrl: NavController,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController,
+    private _http: Http) {
+
+    let loading = _loadingCtrl.create({
+      content: "Buscando novos carros. Aguarde ..."
+    });
+
+    loading.present();
+
+    this._http
+      .get('https://aluracar.herokuapp.com')
+      .map(res => res.json())
+      .toPromise()
+      .then(carros => {
+        this.carros = carros;
+        loading.dismiss();
+      }, err => {
+        loading.dismiss();
+        _alertCtrl.create({
+          title: 'Falha na conexão',
+          message: 'Ocorreu um erro ao realizar a requisição.',
+          buttons: ['Ok']
+        }).present();
+      });
   }
-
 }
