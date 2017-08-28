ef3c711e0a6a5608e72d80448473bde1edf3f4b3
ivanassisleal <ivanassisleal@gmail.com>
2017-08-28 13:28:29 -0300

    ...

--- src/pages/carro/carro.ts
+++ src/pages/carro/carro.ts
@@ -7,8 +7,20 @@ import { NavParams } from 'ionic-angular';
 export class CarroPage {
 
     public carro;
+    public acessorios;
+    private _precoTotal: number;
 
     constructor(public navParams: NavParams) {
         this.carro = navParams.get('carroSelecionado');
+        this._precoTotal = this.carro.preco;
+        this.acessorios = [
+            { nome: 'Freio ABS', preco: 800 },
+            { nome: 'Ar-condicionado', preco: 1000 },
+            { nome: 'MP3 Player', preco: 500 }
+        ];
+    }
+
+    get precoTotal() {
+        return this._precoTotal;
     }
 }
